8a580f99e7d19883bd229f7bf08debb3a89c9df1
Mayank Jangid <226301131@gkv.ac.in>
2025-04-05 16:23:00 +0530

    storyboard for home and product cell

---
+++ Nectar/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  Nectar
+//
+//  Created by Mayank Jangid on 4/5/25.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
